5df2936713725266ea21f8553277ef5afdb0fddc
prashanthavvari <apprashanth55@gmail.com>
2015-11-20 21:36:07 +0530

    Submit Commit

--- src/012sllSort.cpp
+++ src/012sllSort.cpp
@@ -22,5 +22,39 @@ struct node {
 };
 
 void sll_012_sort(struct node *head){
-	
-}
+		int count[3] = { 0 };
+		struct node *p, *q;
+		p = head;
+		q = head;
+		printf("\n");
+		while (q->next != NULL)
+		{
+			if (q->data == 0)
+				count[0]++;
+			else if (q->data == 1)
+				count[1]++;
+			else
+				count[2]++;
+			q = q->next;
+		}
+		count[q->data]++;
+		while (count[0] > 0)
+		{
+			p->data = 0;
+			p = p->next;
+			count[0]--;
+		}
+		while (count[1] > 0)
+		{
+			p->data = 1;
+			p = p->next;
+			count[1]--;
+		}
+		while (count[2] > 0)
+		{
+			p->data = 2;
+			p = p->next;
+			count[2]--;
+		}
+		p = NULL;
+	}
